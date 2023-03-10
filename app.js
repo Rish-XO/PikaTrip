7b936a42da4ec4d1f7ef522dcc80ecfd56ec17ae
Rishal <Rishal.mhd009@gmail.com>
2023-03-10 17:27:05 +0530

    image carousel setup and seeds edit done

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+if (process.env.NODE_ENV != "production") {
+  require('dotenv').config()
+}
+
+console.log(process.env.SECRET);
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
@@ -60,7 +66,7 @@ app.use((req, res, next) => {
 //   if (!['/login', '/register', '/'].includes(req.originalUrl)) {
 //     req.session.returnTo = req.originalUrl;
 // }
-console.log(req.session);
+// console.log(req.session);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
-const cities = require('./cities');
-const {places, decriptors, descriptors} = require('./seedHelpers');
+const cities = require("./cities");
+const { places, decriptors, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
-
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
-
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
@@ -13,26 +11,31 @@ db.once("open", () => {
   console.log("database connected");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for(let i=0; i<50; i++){
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            author:'64025a7ed7d2709deb2d4de6',
-            location : `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/804132',
-            description:'blah blah blah blah blah blah blah blah blah blah blah blah',
-            price
-        
-        })
-        await camp.save();
-    }
-}
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: "64025a7ed7d2709deb2d4de6",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description:
+        "blah blah blah blah blah blah blah blah blah blah blah blah",
+      price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/duws7fze5/image/upload/v1678444271/yelpCamp/y7xitwnmiuhdpxvwv6ut.jpg",
+          filename: "yelpCamp/y7xitwnmiuhdpxvwv6ut",
+        },
+      ],
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    mongoose.connection.close()
-})
+  mongoose.connection.close();
+});
