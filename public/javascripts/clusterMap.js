a6f92c1e85b9c53c205855b46310d3202f1b0606
Rishal <Rishal.mhd009@gmail.com>
2023-03-15 19:09:30 +0530

    styling done , next to security issues check up

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken =
   "pk.eyJ1IjoicmlzaHJhdXoiLCJhIjoiY2xmNnVhdGpuMXE3ZzN2cjA2dms4YTc0dyJ9.NELzHzyfIRdgA5f0PjfG6Q";
 const map = new mapboxgl.Map({
-  container: "map",
+  container: "cluster-map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/light-v11",
 //   india as center 77.970080,27.059978
@@ -9,6 +9,8 @@ const map = new mapboxgl.Map({
   center: [-103.5917, 40.6699],
   zoom: 3,
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on("load", () => {
   // Add a new source from our GeoJSON data and
