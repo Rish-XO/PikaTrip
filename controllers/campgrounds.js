3e229292d9e2442fb32cfed0aa69de5f6d32cdeb
Rishal <Rishal.mhd009@gmail.com>
2023-04-21 16:06:59 +0530

    fixed mutilple form submission

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -7,7 +7,7 @@ const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", { campgrounds });
+  res.render("campgrounds/index", { campgrounds }); 
 };
 
 module.exports.renderNewForm = (req, res) => {
